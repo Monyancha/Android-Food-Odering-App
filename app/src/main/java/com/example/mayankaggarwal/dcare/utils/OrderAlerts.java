87ea237e70c692b1b4dbe6bd8639b02c68ea1f72
mayankagg9722 <mayankagg9722@gmail.com>
2017-06-03 22:18:15 +0530

    added notification item layout

--- app/src/main/java/com/example/mayankaggarwal/dcare/utils/OrderAlerts.java
+++ app/src/main/java/com/example/mayankaggarwal/dcare/utils/OrderAlerts.java
@@ -6,10 +6,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.example.mayankaggarwal.dcare.R;
-import com.example.mayankaggarwal.dcare.activities.Details;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -91,24 +92,40 @@ public class OrderAlerts {
 
     public static void showfeedbackAlert(final Activity activity) {
 
-        final Button deliver, cancel, returned;
+        final Button deliver, cancel, returned, continueDeliver, continueCancel;
+        final LinearLayout deliveryLayout, returnLayout;
+        final ImageView starone, startwo, starthree, starfour, starfive;
         final Spinner reasons;
         List<String> reasonList;
         ArrayAdapter<String> reasonAdapter;
 
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-        View view = activity.getLayoutInflater().inflate(R.layout.alert_feedback_order, null);
+        final View view = activity.getLayoutInflater().inflate(R.layout.alert_feedback_order, null);
         deliver = (Button) view.findViewById(R.id.delivered);
         cancel = (Button) view.findViewById(R.id.cancelled);
         returned = (Button) view.findViewById(R.id.returned);
-        reasons=(Spinner) view.findViewById(R.id.reasonspinner);
+        continueCancel = (Button) view.findViewById(R.id.continuedialog2);
+        continueDeliver = (Button) view.findViewById(R.id.continuedialog1);
+        reasons = (Spinner) view.findViewById(R.id.reasonspinner);
+        deliveryLayout = (LinearLayout) view.findViewById(R.id.deliverlayout);
+        returnLayout = (LinearLayout) view.findViewById(R.id.cancelretunlayout);
+        starone = (ImageView) view.findViewById(R.id.starone);
+        startwo = (ImageView) view.findViewById(R.id.startwo);
+        starthree = (ImageView) view.findViewById(R.id.starthree);
+        starfour = (ImageView) view.findViewById(R.id.starfour);
+        starfive = (ImageView) view.findViewById(R.id.starfive);
 
-        reasonList=new ArrayList<>();
+
+        //initial setup
+        returnLayout.setVisibility(View.GONE);
+        deliveryLayout.setVisibility(View.GONE);
+
+
+        reasonList = new ArrayList<>();
         reasonList.add("Reason 1");
         reasonList.add("Reason 2");
         reasonAdapter = new ArrayAdapter<String>(activity, R.layout.support_simple_spinner_dropdown_item, reasonList);
         reasons.setAdapter(reasonAdapter);
-
 
         deliver.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -163,23 +180,84 @@ public class OrderAlerts {
             }
         });
 
+        continueCancel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        continueCancel.setBackgroundResource(R.drawable.round_shape_solid_grey);
+                        continueCancel.setTextColor(activity.getResources().getColor(R.color.white));
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        continueCancel.setBackgroundResource(R.drawable.round_shape_border_grey);
+                        continueCancel.setTextColor(activity.getResources().getColor(R.color.dark_grey));
+                        return false;
+                }
+                return false;
+            }
+        });
+
+        continueDeliver.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        continueDeliver.setBackgroundResource(R.drawable.round_shape_solid_grey);
+                        continueDeliver.setTextColor(activity.getResources().getColor(R.color.white));
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        continueDeliver.setBackgroundResource(R.drawable.round_shape_border_grey);
+                        continueDeliver.setTextColor(activity.getResources().getColor(R.color.dark_grey));
+                        return false;
+                }
+                return false;
+            }
+        });
+
         deliver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                feedbackAlert.dismiss();
+                setColorTab(cancel, returned, deliver, activity, view);
+                deliveryLayout.setVisibility(View.VISIBLE);
+                returnLayout.setVisibility(View.GONE);
+
+                //setting stars
+                setStarPattern(starone, startwo, starthree, starfour, starfive);
             }
         });
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                feedbackAlert.dismiss();
+                setColorTab(deliver, returned, cancel, activity, view);
+                deliveryLayout.setVisibility(View.GONE);
+                returnLayout.setVisibility(View.VISIBLE);
             }
         });
 
         returned.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setColorTab(cancel, deliver, returned, activity, view);
+                deliveryLayout.setVisibility(View.GONE);
+                returnLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        continueCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                continueCancel.setBackgroundResource(R.drawable.round_shape_solid_grey);
+                continueCancel.setTextColor(activity.getResources().getColor(R.color.white));
+                feedbackAlert.dismiss();
+            }
+        });
+
+        continueDeliver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                continueDeliver.setBackgroundResource(R.drawable.round_shape_solid_grey);
+                continueDeliver.setTextColor(activity.getResources().getColor(R.color.white));
                 feedbackAlert.dismiss();
             }
         });
@@ -190,6 +268,106 @@ public class OrderAlerts {
         feedbackAlert.show();
         if (activity.isDestroyed() || activity.isFinishing()) {
             feedbackAlert.dismiss();
+        }
+    }
+
+    private static void setStarPattern(final ImageView starone, final ImageView startwo, final ImageView starthree, final ImageView starfour, final ImageView starfive) {
+
+        starone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starone.setImageResource(R.drawable.selectstar);
+                startwo.setImageResource(R.drawable.notselectstar);
+                starthree.setImageResource(R.drawable.notselectstar);
+                starfour.setImageResource(R.drawable.notselectstar);
+                starfive.setImageResource(R.drawable.notselectstar);
+            }
+        });
+        startwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starone.setImageResource(R.drawable.selectstar);
+                startwo.setImageResource(R.drawable.selectstar);
+                starthree.setImageResource(R.drawable.notselectstar);
+                starfour.setImageResource(R.drawable.notselectstar);
+                starfive.setImageResource(R.drawable.notselectstar);
+            }
+        });
+        starthree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starone.setImageResource(R.drawable.selectstar);
+                startwo.setImageResource(R.drawable.selectstar);
+                starthree.setImageResource(R.drawable.selectstar);
+                starfour.setImageResource(R.drawable.notselectstar);
+                starfive.setImageResource(R.drawable.notselectstar);
+            }
+        });
+        starfour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starone.setImageResource(R.drawable.selectstar);
+                startwo.setImageResource(R.drawable.selectstar);
+                starthree.setImageResource(R.drawable.selectstar);
+                starfour.setImageResource(R.drawable.selectstar);
+                starfive.setImageResource(R.drawable.notselectstar);
+            }
+        });
+        starfive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starone.setImageResource(R.drawable.selectstar);
+                startwo.setImageResource(R.drawable.selectstar);
+                starthree.setImageResource(R.drawable.selectstar);
+                starfour.setImageResource(R.drawable.selectstar);
+                starfive.setImageResource(R.drawable.selectstar);
+            }
+        });
+    }
+
+
+    private static void setColorTab(Button one, Button two, Button three, Activity activity, View view) {
+
+        final String s_one = activity.getResources().getResourceEntryName(one.getId());
+        final String s_two = activity.getResources().getResourceEntryName(two.getId());
+        final String s_three = activity.getResources().getResourceEntryName(three.getId());
+
+        checkingIDSBack(s_one, activity, view);
+        checkingIDSBack(s_two, activity, view);
+        checkingIDS(s_three, activity, view);
+    }
+
+    private static void checkingIDSBack(String string, Activity activity, View view) {
+        final Button deliver, cancel, returned;
+        deliver = (Button) view.findViewById(R.id.delivered);
+        cancel = (Button) view.findViewById(R.id.cancelled);
+        returned = (Button) view.findViewById(R.id.returned);
+        if (string.equals("delivered")) {
+            deliver.setBackgroundResource(R.drawable.round_shape_border_blue);
+            deliver.setTextColor(activity.getResources().getColor(R.color.themeblue));
+        } else if (string.equals("cancelled")) {
+            cancel.setBackgroundResource(R.drawable.round_shape_border_orange);
+            cancel.setTextColor(activity.getResources().getColor(R.color.themered));
+        } else if (string.equals("returned")) {
+            returned.setBackgroundResource(R.drawable.round_shape_border_grey);
+            returned.setTextColor(activity.getResources().getColor(R.color.dark_grey));
+        }
+    }
+
+    private static void checkingIDS(String string, Activity activity, View view) {
+        final Button deliver, cancel, returned;
+        deliver = (Button) view.findViewById(R.id.delivered);
+        cancel = (Button) view.findViewById(R.id.cancelled);
+        returned = (Button) view.findViewById(R.id.returned);
+        if (string.equals("delivered")) {
+            deliver.setBackgroundResource(R.drawable.round_solid_blue);
+            deliver.setTextColor(activity.getResources().getColor(R.color.white));
+        } else if (string.equals("cancelled")) {
+            cancel.setBackgroundResource(R.drawable.round_shape_solid_invalid);
+            cancel.setTextColor(activity.getResources().getColor(R.color.white));
+        } else if (string.equals("returned")) {
+            returned.setBackgroundResource(R.drawable.round_shape_solid_grey);
+            returned.setTextColor(activity.getResources().getColor(R.color.white));
         }
     }
 
