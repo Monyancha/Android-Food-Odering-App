bb2c5b8127acd913eab980e0086865feddc16951
mayankagg9722 <mayankagg9722@gmail.com>
2017-06-05 00:12:04 +0530

    scroll added

--- app/src/main/java/com/example/mayankaggarwal/dcare/fragments/OrderFragment.java
+++ app/src/main/java/com/example/mayankaggarwal/dcare/fragments/OrderFragment.java
@@ -3,6 +3,7 @@ package com.example.mayankaggarwal.dcare.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,7 +33,7 @@ public class OrderFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_order, container, false);
-        OrderAlerts.showfeedbackAlert(getActivity());
+//        OrderAlerts.showfeedbackAlert(getActivity());
         return view;
     }
 
